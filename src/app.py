d562be4265f8df191fd18fff27b999e9678fc270
Jorge Lopez <jorgelopezseijas@gmail.com>
2020-09-04 13:52:11 +0200

    Añadido redireccion para que a home apunte a /api/v1/galineiro/

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from .config import app_config
 from .routes.v1 import galineiro
 import os
@@ -9,3 +9,8 @@ env_name = os.getenv('FLASK_ENV') or 'production'
 app.config.from_object(app_config[env_name])
 
 app.register_blueprint(galineiro, url_prefix='/api/v1/galineiro')
+
+
+@app.route("/")
+def index():
+    return redirect(url_for('galineiro.index'))
