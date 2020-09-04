33d22780f7edf5d981713707b9fbcf260175b032
Jorge Lopez <jorgelopezseijas@gmail.com>
2020-09-04 13:34:59 +0200

    Substituido o ficheiro coas credencias config.json por variables de entorno

--- src/routes/v1/auth.py
+++ src/routes/v1/auth.py
@@ -1,22 +1,19 @@
 from functools import wraps
 from flask import request, Response
-import json
+import os
 
-with open('src/routes/v1/config.json', 'r') as f:
-    config = json.load(f)
-
-username = config['auth']['username']
-password = config['auth']['password']
 
 def check(usernameIn, passwordIn):
-    return usernameIn == username and passwordIn == password
+    return usernameIn == os.getenv('GALINEIRO_USER') and passwordIn == os.getenv('GALINEIRO_PASSWORD')
+
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
     return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 
 def basic(f):
     @wraps(f)
