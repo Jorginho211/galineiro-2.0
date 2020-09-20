478a204cef71eb5d02a8e5985ae0898296df4d5f
Jorge Lopez <jorgelopezseijas@gmail.com>
2020-09-20 22:58:05 +0200

    Engadido wsgi.py e gunicorn nas dependencias

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from src.controllers import CoreGalineiro
+from src.app import app
+import threading
+import os
+
+load_dotenv()
+
+CoreGalineiro.instance().root_path = os.path.dirname(
+                                            os.path.abspath(__file__))
+ciclo_thread = threading.Thread(target=CoreGalineiro.instance().ciclo)
+ciclo_thread.start()
+
+ciclo_thread_mobil = threading.Thread(
+    target=CoreGalineiro.instance().comprobacion_estado_manual_mobil)
+ciclo_thread_mobil.start()
+
+if __name__ == "__main__":
+    app.run()
